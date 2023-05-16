675e4ddf2fa0d3eb62424a6f6d3ba4c43cc78d02
ptrisha <104321566+ptrisha@users.noreply.github.com>
2023-05-17 07:37:30 +0800

    Use itertools permutations function in challenge3 program

--- code/challenge3.py
+++ code/challenge3.py
@@ -1,44 +1,11 @@
+# Challenge 3
 
-# permutations function - 
-# source: https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
-
-# Python function to print permutations of a given list
-def permutation(lst):
- 
-    # If lst is empty then there are no permutations
-    if len(lst) == 0:
-        return []
- 
-    # If there is only one element in lst then, only
-    # one permutation is possible
-    if len(lst) == 1:
-        return [lst]
- 
-    # Find the permutations for lst if there are
-    # more than 1 characters
- 
-    l = [] # empty list that will store current permutation
- 
-    # Iterate the input(lst) and calculate the permutation
-    for i in range(len(lst)):
-       m = lst[i]
- 
-       # Extract lst[i] or m from the list.  remLst is
-       # remaining list
-       remLst = lst[:i] + lst[i+1:]
- 
-       # Generating all permutations where m is first
-       # element
-       for p in permutation(remLst):
-           l.append([m] + p)
-    return l
- 
- 
+from itertools import permutations
 
 data = list('1867')
-perms = permutation(data)
+perms = list( permutations(data) )
 perms_int = [ int("".join(p)) for p in perms]
-print("number of permutations: ", len(perms))
+print("Number of permutations:", len(perms))
 
 # find permutations that are divisible by 7
 div_7 = []
